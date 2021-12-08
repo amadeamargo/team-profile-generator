649b99c65b4fac0c37b5ee7d990b975b902c8556
amadeamargo <amadeamargo@gmail.com>
2021-12-07 19:58:11 -0600

    added prompts for employee list to index.js

--- index.js
+++ index.js
@@ -63,15 +63,15 @@ const managerPrompt = () => {
     })
 }
 
-const Engineer = () => {
+const engineerPrompt = () => {
     inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "Who is your team's manager?",
+            message: "Please enter engineer's name",
             validate: (data) => {
                 if (data ===''){
-                    return`Please enter manager's name`
+                    return `Input field cannot be left blank. Please enter engineer's name.`
                 }
                 return true
             }
@@ -79,10 +79,10 @@ const Engineer = () => {
         {
             type: "input",
             name: "id",
-            message: "What is the manager's ID",
+            message: "Please enter engineer's ID",
             validate: (data) => {
                 if (data ===''){
-                    return`Please enter manager's ID`
+                    return`Input field cannot be left blank. Please enter engineer's ID.`
                 }
                 return true
             }
@@ -90,28 +90,114 @@ const Engineer = () => {
         {
             type: "input",
             name: "email",
-            message: "What is the manager's email address?",
+            message: "Please enter engineer's email address",
             validate: (data) => {
                 if (data ===''){
-                    return`Please enter manager's email address`
+                    return`Input field cannot be left blank. Please enter engineer's email address.`
                 }
                 return true
             }
         },
         {
             type: "input",
-            name: "officeNumber",
-            message: "Who is the manager's office number?",
+            name: "gitHub",
+            message: "Please enter engineer's gitHub account",
             validate: (data) => {
                 if (data ===''){
-                    return`Please enter manager's office Number`
+                    return`Input field cannot be left blank. Please enter engineer's gitHub account.`
                 }
                 return true
             }
         }
     ]).then (data => {
-        const manager = newManager (data.name, data.id, data.email, data.officeNumber);
-        team.push(manager);
+        const engineer = newEngineer (data.name, data.id, data.email, data.gitHub);
+        team.push(engineer);
         addMenu();
     })
 }
+
+const internPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter intern's name",
+            validate: (data) => {
+                if (data ===''){
+                    return `Input field cannot be left blank. Please enter intern's name.`
+                }
+                return true
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Please enter intern's ID",
+            validate: (data) => {
+                if (data ===''){
+                    return`Input field cannot be left blank. Please enter intern's ID.`
+                }
+                return true
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter intern's email address",
+            validate: (data) => {
+                if (data ===''){
+                    return`Input field cannot be left blank. Please enter intern's email address.`
+                }
+                return true
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Please enter name of intern's school",
+            validate: (data) => {
+                if (data ===''){
+                    return`Input field cannot be left blank. Please enter name of intern's school.`
+                }
+                return true
+            }
+        }
+    ]).then (data => {
+        const intern = newIntern (data.name, data.id, data.email, data.school);
+        team.push(intern);
+        addMenu();
+    })
+}
+
+const addMenu = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "newEmployee",
+            message: "Add a new employee?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Completed employee list"
+            ]
+        }
+    ]).then (data => {
+        switch (data.newEmployee){
+            case "Engineer":
+                engineerPrompt();
+                break;
+                case "Intern";
+                internPrompt();
+                break;
+                case "Completed cmploye list":
+                    console.log(team);
+                    let html = renderHTML (team);
+                    fs.writeFile(".dist/team.html", html, (err)=>
+                    err ? console.log(err) : console.log(`HTML file created`))
+                    break;
+        }
+    })
+}
+
+managerPrompt();
+
